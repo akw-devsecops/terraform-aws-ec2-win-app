fe396c88c277eedb18fe658344c27417160da5f6
Janosch Schäfer <97673513+JanoschSchaefer@users.noreply.github.com>
2023-09-01 12:12:35 +0200

    fix array access

--- codedeploy.tf
+++ codedeploy.tf
@@ -8,7 +8,7 @@ resource "aws_codedeploy_app" "ec2_app" {
 resource "aws_codedeploy_deployment_group" "ec2_app" {
   count = var.create_codedeploy ? 1 : 0
   
-  app_name              = aws_codedeploy_app.ec2_app.name
+  app_name              = aws_codedeploy_app.ec2_app[0].name
   deployment_group_name = local.fq_app_name
   service_role_arn      = aws_iam_role.code_deploy_role.arn
 
