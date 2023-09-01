8445bd155efb3cb5bb1f520ecea63a9bee74711e
Janosch Schäfer <97673513+JanoschSchaefer@users.noreply.github.com>
2023-09-01 12:12:43 +0200

    fix array access

--- codedeploy.tf
+++ codedeploy.tf
@@ -10,7 +10,7 @@ resource "aws_codedeploy_deployment_group" "ec2_app" {
   
   app_name              = aws_codedeploy_app.ec2_app[0].name
   deployment_group_name = local.fq_app_name
-  service_role_arn      = aws_iam_role.code_deploy_role.arn
+  service_role_arn      = aws_iam_role.code_deploy_role[0].arn
 
   ec2_tag_set {
     ec2_tag_filter {
