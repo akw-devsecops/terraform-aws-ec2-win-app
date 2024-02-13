697da48f1d7f55c8f4ab8c2f23d086f1c85412d8
jschaefe-akw <janosch.schaefer@karcher.com>
2024-02-13 13:26:19 +0100

    Fix conditional creation

--- waf.tf
+++ waf.tf
@@ -1,5 +1,5 @@
 resource "aws_wafv2_web_acl_association" "this" {
-  count = var.create_lb && var.attach_waf_to_alb ? 1 : 0
+  count = var.attach_waf_to_alb ? 1 : 0
 
   resource_arn = module.alb_http[0].lb_arn
   web_acl_arn  = var.web_acl_arn
